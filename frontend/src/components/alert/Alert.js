8922bcfa999057742b7826b3e812849a23fdadb9
Muhammad Fahad <106611736+MFahad11@users.noreply.github.com>
2023-03-03 17:51:57 +0500

    first commit

--- frontend/src/components/alert/Alert.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-const Alert=({variant})=>{
-    return(
-        <div className="alert alert-danger" role="alert">
-            {variant}
-        </div>
-    )
-}
-export default Alert
